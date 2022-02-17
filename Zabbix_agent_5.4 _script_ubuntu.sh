87df6340f8a6ff52c627b9687b58c44f1943d7b8
navalbaliyan <32090746+navalbaliyan@users.noreply.github.com>
2022-02-17 16:37:30 +0530

    Update Zabbix_agent_5.4 _script_ubuntu.sh

--- Zabbix_agent_5.4 _script_ubuntu.sh
+++ Zabbix_agent_5.4 _script_ubuntu.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+sudo -i
 apt update -y && apt upgrade -y
 wget https://repo.zabbix.com/zabbix/5.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_5.4-1+ubuntu20.04_all.deb
 dpkg -i zabbix-release_5.4-1+ubuntu20.04_all.deb
 apt update -y
 apt install zabbix-agent -y
-echo 'Hostname=$(hostname -f)' > /etc/zabbix/zabbix_agentd.conf
+name=$(hostname -f)
+echo Hostname=$name > /etc/zabbix/zabbix_agentd.conf
 echo 'PidFile=/run/zabbix/zabbix_agentd.pid' >> /etc/zabbix/zabbix_agentd.conf
 echo 'LogFile=/var/log/zabbix/zabbix_agentd.log' >> /etc/zabbix/zabbix_agentd.conf
 echo 'LogFileSize=0' >> /etc/zabbix/zabbix_agentd.conf
@@ -16,5 +18,3 @@ systemctl restart zabbix-agent
 systemctl enable zabbix-agent
 apt install firewalld -y
 firewall-cmd --add-port=10050/tcp
-systemctl restart firewalld
-systemctl enable firewalld
